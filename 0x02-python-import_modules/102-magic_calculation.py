9374bbae30f5fe4f331e704c13f23fc741c26d5f
PunchArm255 <simohamednassiri@gmail.com>
2024-01-07 08:39:45 -0800

    Task #8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
+    """I HATE PEP8"""
     from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
-
+        return c
     else:
-        return(sub(a, b))
+        return sub(a, b)
