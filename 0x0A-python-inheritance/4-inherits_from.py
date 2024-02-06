a077e0560b59d518d3d36f9b2edff3187b2323a2
PunchArm255 <simohamednassiri@gmail.com>
2024-02-05 18:12:29 -0800

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+This is '4-inherits_from' module.
+Functions and Classes:
+    def inherits_from(obj, a_class)
+"""
+
+
 def inherits_from(obj, a_class):
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    """
+    checking if object is instance of class that inherited
+    (directly/indirectly) from specified class
+    """
+    related = issubclass(obj.__class__, a_class)
+    not_same_class = (obj.__class__ is not a_class)
+
+    return (related and not_same_class)
