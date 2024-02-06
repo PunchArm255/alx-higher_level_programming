22663ce66daff9077a2c0584743fd9e08d81f246
PunchArm255 <simohamednassiri@gmail.com>
2024-02-05 18:06:48 -0800

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+This is '0-lookup' module.
+Functions and Classes:
+    def lookup(obj)
+"""
+
+
 def lookup(obj):
+    """
+    returns list of dispo attributes/methods of object
+    """
     return dir(obj)
