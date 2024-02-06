ae3ac87b282d5a879ff56cb2f355ec6a6f3849fb
PunchArm255 <simohamednassiri@gmail.com>
2024-02-05 18:19:54 -0800

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+"""
+This is '10-square' module.
+Functions and Classes:
+    class Square
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """do a square bro"""
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
