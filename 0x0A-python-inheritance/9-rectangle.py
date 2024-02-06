f5382bc85c82b1734bef3c99f4d5d2d8776b8815
PunchArm255 <simohamednassiri@gmail.com>
2024-02-05 18:17:02 -0800

    task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
+"""
+This is '9-rectangle' module.
+Functions and Classes:
+    class Rectangle
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """do a reccy lollol"""
     def __init__(self, width, height):
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
+        """calculate surf of a reccy"""
         return self.__width * self.__height
 
     def __str__(self):
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
-        return string
+        """txt reppy of reccy"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
