2e1287f6f154733e4d5eed75a9eb27d73324779a
PunchArm255 <simohamednassiri@gmail.com>
2024-02-05 18:09:17 -0800

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+This is '1-my_list' module.
+Functions and Classes:
+    class MyList
+"""
+
+
 class MyList(list):
+    """
+    subclass who inherits from the 'list' class
+    """
     def print_sorted(self):
+        """print sorted tablo in an ascending order"""
         print(sorted(self))
