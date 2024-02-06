94af6fa9ab6aaf0df87a0353e1c828ed76f1196c
PunchArm255 <simohamednassiri@gmail.com>
2024-02-05 18:18:33 -0800

    task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-class BaseGeometry:
+"""
+This is '6-base_geometry' module.
+Functions and Classes:
+    class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """evolved geo class prolly"""
     def area(self):
+        """spawn exception"""
         raise Exception("area() is not implemented")
