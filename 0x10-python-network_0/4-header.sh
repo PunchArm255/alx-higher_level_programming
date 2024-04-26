cfb07bb462b9736f8b2afdbdd07084928f36978e
Your Name <you@example.com>
2024-04-25 19:49:04 -0700

    task 4 update

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send get request to given url with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# Send get request to a given url with a header variable
+curl -sH "X-School-User-Id: 98" "$1"
