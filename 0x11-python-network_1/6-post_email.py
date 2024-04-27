82a3903053b0a41957dd0353ad591b6f7b458175
Simo Nassiri <simohamednassiri@gmail.com>
2024-04-26 19:38:16 -0700

    task 6 update

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""This scripty
-- takes URL,
-- sends request to URL and displays the value
-- of X-Request-Id variable found in the response header
 """
-import sys
-import urllib.request
+This script send POST request to given URL
+(using 'requests' pckge)
+"""
 
-if __name__ == "__main__":
-    url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+def main():
+    """
+    Send POST request to URL with email parameter &
+    display response body (using 'requests' pkg)
+    """
+    import requests
+    from sys import argv
+
+    URL, email = argv[1], argv[2]
+    res = requests.post(URL, data={'email': email})
+    print(res.text)
+
+
+if __name__ == '__main__':
+    main()
